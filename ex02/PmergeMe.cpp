6448f3ad562d2f032c7efdab9981cefbd49bcd27
Youssef Chahbi <ychahbi@e2r1p3.1337.ma>
2024-02-11 06:23:11 +0100

    hello_i_am_done

--- ex02/PmergeMe.cpp
+++ ex02/PmergeMe.cpp
@@ -6,11 +6,12 @@
 /*   By: ychahbi <ychahbi@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/08 10:59:09 by ychahbi           #+#    #+#             */
-/*   Updated: 2024/02/08 13:43:57 by ychahbi          ###   ########.fr       */
+/*   Updated: 2024/02/11 06:09:04 by ychahbi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "PmergeMe.hpp"
+#include <algorithm>
 
 PmergeMe::PmergeMe(){}
 
@@ -22,30 +23,178 @@ void    PmergeMe::set(std::string str)
 void    PmergeMe::error(std::string error)
 {
     std::cout << error << std::endl;
+    exit(1);
+}
+
+void    check(const char *str)
+{
+    for (int i = 0; str[i] != '\0'; i++)
+        if (!(isdigit(str[i])))
+            PmergeMe::error("Error in parsing");
 }
 
 void    PmergeMe::parssing()
 {
     std::stringstream input(av);
     std::string tmp;
-    std::stringstream conv;
     int               int_tmp;
 
     while(std::getline(input, tmp, ' '))
     {
-        conv << tmp;
+        check(tmp.c_str());
+        std::stringstream conv( tmp);
         conv >> int_tmp;
-        if (int_tmp >= 0 && int_tmp < INT_MAX)
+        if (int_tmp < INT_MAX)
             funkctro.push_back(int_tmp);
         else
             error("Error in parssing!");
     }
 }
 
+void    PmergeMe::fill_pair()
+{
+    start = clock(); 
+    (funkctro.size() % 2 == 0) ? odd = -1 : odd = *(funkctro.end() - 1);
+    
+    std::vector<int>::iterator it;
+    it = funkctro.begin();
+    while (it != funkctro.end() && funkctro.size() > 1)
+    {
+        if (funkctro.size() > 1)
+        {
+            int tmp = *it;
+            funkctro.erase(it);
+            pair.push_back(std::make_pair(tmp, *it));
+            funkctro.erase(it);
+        }
+    }
+}
+
+void    PmergeMe::swap(int& a, int& b)
+{
+    int tmp = a;
+    a = b;
+    b = tmp;
+}
+
+void    PmergeMe::sort_pairs()
+{
+    std::vector<std::pair<int, int> >::iterator its;
+    its = pair.begin();
+
+    while (its != pair.end())
+    {
+        if (its->first < its->second)
+            swap(its->first, its->second);
+        its++;
+    }
+    std::sort(pair.begin(), pair.end());
+}
+
+void    PmergeMe::makeChain()
+{
+    if (pair.size() > 0)
+    {
+        std::vector<std::pair<int, int> >::iterator its;
+        its = pair.begin();
+
+        chain.push_back(its->second);
+        chain.push_back(its->first);
+        its++;
+        while (its != pair.end())
+        {
+            chain.push_back(its->first);
+            pend.push_back(its->second);
+            its++;
+        }
+    }
+}
+
+void    PmergeMe::genJacobsthal(int limit)
+{
+    int first = 0;
+    int second = 1;
+    int next;
+
+    while (first <= limit + 2)
+    {
+        Jacobsthal.push_back(first);
+        next = first * 2 + second;
+        first = second;
+        second = next;
+    }
+}
+
+void        PmergeMe::genCombination()
+{
+    std::vector<int>::iterator it;
+    it = std::find(Jacobsthal.begin(), Jacobsthal.end(), 3);;
+    int stop = 1;
+    int it_tmp;
+
+    while (it != Jacobsthal.end())
+    {
+        it_tmp = *it;
+        while (it_tmp != stop)
+        {
+            Combination.push_back(it_tmp);
+            it_tmp--;
+        }
+        stop = *it;
+        it++;
+    }
+}
+
+void    PmergeMe::binarySearch()
+{
+    std::vector<int>::iterator it;
+    std::vector<int>::iterator finder;
+    it = Combination.begin();
+    while (it != Combination.end())
+    {
+        if (*it - 2 < (int)pend.size())
+        {
+            int tmp = *it - 2;
+            finder = std::lower_bound(chain.begin(), chain.end(), pend[tmp]);
+            chain.insert(finder, pend[tmp]);
+        }
+        it++;
+    }
+    if (odd != -1)
+    {
+        finder = std::lower_bound(chain.begin(), chain.end(), odd);
+        chain.insert(finder, odd);
+    }
+    end = clock();
+}
+
+void    PmergeMe::displayChain()
+{
+    std::vector<int>::iterator it;
+
+    it = chain.begin();
+    while(it != chain.end())
+    {
+        std::cout << *it << " ";
+        it++;
+    }
+    std::cout << std::endl;
+    std::cout << "Time to process a range of " << chain.size() << " elements with std::clock_t : ";
+    double time = (double)(end - start) / CLOCKS_PER_SEC * 1000000.0;
+    std::cout << time << " us" << std::endl;
+}
+
 PmergeMe::PmergeMe(std::string av)
 {
     set(av);
     parssing();
+    fill_pair();
+    sort_pairs();
+    makeChain();
+    genJacobsthal(pend.size() * 2);
+    genCombination();
+    binarySearch();
+    displayChain();
 }
 
 PmergeMe::~PmergeMe(){}
--- ex02/PmergeMe.hpp
+++ ex02/PmergeMe.hpp
@@ -6,7 +6,7 @@
 /*   By: ychahbi <ychahbi@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/08 10:59:08 by ychahbi           #+#    #+#             */
-/*   Updated: 2024/02/08 13:40:21 by ychahbi          ###   ########.fr       */
+/*   Updated: 2024/02/11 05:50:37 by ychahbi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,18 +18,36 @@
 #include <string>
 #include <limits>
 #include <sstream>
+#include <ctime>
+
 
 class PmergeMe
 {
     private:
-        std::string         av;
-        std::vector<int>    funkctro;
+        std::string                       av;
+        std::vector<int>                  funkctro;
+        std::vector<std::pair<int, int> > pair;
+        std::vector<int>                  chain;
+        std::vector<int>                  pend;
+        std::vector<int>                  Jacobsthal;
+        std::vector<int>                  Combination;
+        std::clock_t                      start;
+        std::clock_t                      end;
+        int                               odd;
     public:
         PmergeMe();
         PmergeMe(std::string);
         static  void        error(std::string);
         void        set(std::string);
         void        parssing();
+        void        fill_pair();
+        void        sort_pairs();
+        void        swap(int&, int&);
+        void        makeChain();
+        void        genJacobsthal(int);
+        void        genCombination();
+        void        binarySearch();
+        void        displayChain();
         ~PmergeMe();
 };
 
